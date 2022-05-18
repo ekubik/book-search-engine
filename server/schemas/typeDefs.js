2c04c60563e331a6d97e91e5ddae5e8c095580e7
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-19 01:26:00 +1000

    debug typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,10 +28,10 @@ type Query {
 }
 
 type Mutation {
-    login (email: String!, password: String!): Auth
-    addUser (username: String!, email: String!, password: String!): Auth
-    saveBook (bookId: ID!, authors: [String], description: String!, title: String!, image: String, link: String): User
-    removeBook (bookId: ID:! ): User 
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: ID!, authors: [String], description: String!, title: String!, image: String, link: String): User
+    removeBook(bookId: ID! ): User 
 
 }
 `;
