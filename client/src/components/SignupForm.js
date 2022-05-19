cd9796e74f195f239a3854a3f14aa3062088e353
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-19 22:32:46 +1000

    sign up

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import {useMutation} from "@apollo/client";
+import {ADD_USER} from "../utils/mutations";
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -10,7 +11,7 @@ const SignupForm = () => {
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
-  const [showAlert, setShowAlert] = useState(false);
+  const [addUser, {error}]= useMutation({ADD_USER});
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,17 +29,10 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
-    } catch (err) {
-      console.error(err);
+     const {data} = await addUser({ variables: {...userFormData},});
+     Auth.login(data.addUser.token);
+    } catch (error) {
+      console.error(error);
       setShowAlert(true);
     }
 
