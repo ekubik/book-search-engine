da2c3390e51c30e97fcd3f1ebf1f93ddade14545
Ewa Kubik <ewa.kubik@hotmail.com>
2022-05-19 23:03:55 +1000

    latest version

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -4,8 +4,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation} from "@apollo/client";
 
 
-import { LOGIN_USER} from "../utils/mutations"
-import { loginUser } from '../utils/API';
+import { LOGIN_USER} from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
